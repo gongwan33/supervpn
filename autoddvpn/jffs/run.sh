9b8a50b744fba4addd20306f9c22cffaf5712c6e
wanggong <gongwan33@gmail.com>
2014-09-20 19:05:22 +0800

    add nvram client function

--- autoddvpn/jffs/run.sh
+++ autoddvpn/jffs/run.sh
@@ -4,31 +4,39 @@ VPNUP='vpnup.sh'
 VPNDOWN='vpndown.sh'
 VPNLOG='/tmp/autoddvpn.log'
 IPUP="/tmp/pptpd_client/vpn"
+TEMP_LK=0
+
+PPTP_ENABLE=$(nvram get pptpd_client_enable)
+
 
 while [ 1 -eq 1 ]
 do
-  echo "Finding vpn..." >> $VPNLOG
-  while [ 1 -eq 1 ]
-  do 
-   ifconfig ppp1; 
-   if [ 0 -eq $? ]; then
-     sleep 10
-     /tmp/vpnup.sh pptp
-     break
-   fi 
-   sleep 20
-  done
- 
-  echo "Waiting vpn exit..." >> $VPNLOG
-  while [ 1 -eq 1 ]
-  do
-   ifconfig ppp1; 
-   if [ 0 -ne $? ]; then
+  PPTP_ENABLE=$(nvram get pptpd_client_enable)
+  if [ 0 -ne $PPTP_ENABLE ]; then
+    echo "Founding vpn..." >> $VPNLOG
+    while [ 1 -eq 1 ]
+    do  
+     if [ -f $IPUP ]; then
+       sleep 10
+       echo "Vpn connected. Route info adding..." >> $VPNLOG
+       /tmp/vpnup.sh pptp
+       TEMP_LK=0
+       echo "Route info added." >> $VPNLOG
+       break
+     fi 
+     sleep 20
+    done
+   fi
+    
+   if [ ! -f $IPUP ]; then
      sleep 5
-     /tmp/vpndown.sh pptp
-     break
+     if [ 0 -eq $TEMP_LK ]; then
+       echo "Vpn drop. Route info clearing..." >> $VPNLOG 
+       /tmp/vpndown.sh pptp
+       TEMP_LK=1
+       echo "Vpn drop. Route info cleared." >> $VPNLOG 
+       break
+     fi       
    fi
    sleep 20
-  done
 done
-
